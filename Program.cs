ddff7bde60c4bd4f34469ee1363e2ec13967a654
Artemy Aprelikov <aprel1415@yandex.ru>
2025-09-22 07:12:51 +0300

    calculator with simple operations

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
 ﻿using practice;
 
-Console.WriteLine("Please, input your name here: ");
-string? name = Console.ReadLine();
-Console.WriteLine("Please, input your age here: ");
-string? ageInt = Console.ReadLine();
-int.TryParse(ageInt, out int age);
-Person artemy = new Person(name ?? "Unknown", age);
-Console.WriteLine(artemy);
+// Calculator
+string? numberOne = Console.ReadLine();
+int.TryParse(numberOne, out int firstNum);
+string? numberTwo = Console.ReadLine();
+int.TryParse(numberTwo, out int secondNum);
+Console.WriteLine("Choose operation:");
+string? operationNumber = Console.ReadLine();
+int.TryParse(operationNumber, out int chosedOperation);
+switch (chosedOperation)
+{
+    case 1:
+        int answerForPlus = firstNum + secondNum;
+        Console.WriteLine(answerForPlus);
+        return answerForPlus;
+    case 2:
+        int answerForMin = firstNum - secondNum;
+        Console.WriteLine(answerForMin);
+        return answerForMin;
+    case 3:
+        int answerForMul = firstNum * secondNum;
+        Console.WriteLine(answerForMul);
+        return answerForMul;
+    case 4:
+        if (secondNum == 0)
+        {
+            Console.WriteLine("Error!");
+            return 0;
+        }
+
+        int answerForDec = firstNum / secondNum;
+        Console.WriteLine(answerForDec);
+        return answerForDec;
+    default:
+        return 0;
+}
